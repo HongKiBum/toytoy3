d22c1615f2d918aeb1c4d8908187ed6986f88355
HongKiBum <hgb9720@hanyang.ac.kr>
2024-12-02 16:36:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,19 @@ setup(
     name="fun-tools",
     version="1.0.0",
     description="A collection of fun games and utilities",
-    author="HongKiBum",
-    author_email="hgb9720@hanyang.ac.kr",
+    author="Hong Ki Bum",
+    author_email="your_email@example.com",
     url="https://github.com/HongKiBum/toytoy3",
     packages=find_packages(),
     install_requires=[
-        "kivy",            # 필요한 패키지
-        "mediapipe",       # 필요한 패키지
-        "ultralytics",     # 필요한 패키지
+        "kivy>=2.0.0",       # Kivy 라이브러리
+        "mediapipe",         # Mediapipe 라이브러리
+        "ultralytics"        # Ultralytics YOLO 라이브러리
     ],
     python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
